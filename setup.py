75224f8cc4734c08f91be5ae52bad4e28577eb02
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-10-23 01:21:20 +0300

    [Chore](Build): Removed console env check from setup just to be sure.

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
 
-if __name__ == "__main__":
-    setup()
+# if __name__ == "__main__":
+"""
+Usage instructions:
+- If you are installing: `python setup.py install`
+- If you are developing: `python setup.py sdist --format=zip bdist_wheel --universal bdist_wininst && twine check dist/*`
+"""
+setup()
