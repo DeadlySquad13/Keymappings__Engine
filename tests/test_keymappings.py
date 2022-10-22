17fa5d494b1b78bf7f2bb92729fab9d7a741d0a8
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-10-23 01:13:40 +0300

    [Test](Keymappings): Covered decoder and parser.

--- tests/test_keymappings.py
+++ tests/test_keymappings.py
@@ -1,17 +1,19 @@
+from typing import List
 from pynput import keyboard
+import pytest
 
 from keymappings.main import Keymapping, match_keymappings
 
 def test_answer():
     km = Keymapping()
-
-    km.add_keymappings({
-        "key_sequences": [
+    keymapping = {
+            "key_sequences": [
             [{keyboard.Key.ctrl_l, keyboard.KeyCode(char="w")}],
             [{keyboard.Key.ctrl_l, keyboard.KeyCode(char="W")}],
         ],
         "command": "TEST",
-    })
+    }
+    km.add_keymappings(keymapping)
 
     matched_sequences = [0 for _ in km.KEYMAPPINGS]
 
@@ -19,5 +21,23 @@ def test_answer():
     assert match_keymappings(keyboard.Key.ctrl_l,
                              km.KEYMAPPINGS, matched_sequences) == []
 
-    assert match_keymappings(keyboard.KeyCode.from_vk(87), # 'w'
-                             km.KEYMAPPINGS, matched_sequences) == km.KEYMAPPINGS
+    assert match_keymappings(keyboard.KeyCode.from_vk(87), # Added 'w'.
+                             km.KEYMAPPINGS, matched_sequences) == [keymapping]
+
+def test_parse_keymapping_str_notation():
+    km = Keymapping()
+
+    assert km.parse_keymapping_str_notation('ctrl_l + w') == { 'ctrl_l', 'w' }
+
+@pytest.mark.parametrize('keymapping,decoded_keymapping', [
+    ('ctrl_l', { keyboard.Key.ctrl_l }),
+    ('w', { keyboard.KeyCode(char='xw') }),
+
+    ('ctrl_l+w', { keyboard.Key.ctrl_l, keyboard.KeyCode(char='w') }),
+    ('ctrl_l +w', { keyboard.Key.ctrl_l, keyboard.KeyCode(char='w') }),
+    ('ctrl_l+ w', { keyboard.Key.ctrl_l, keyboard.KeyCode(char='w') }),
+    ('ctrl_l + w', { keyboard.Key.ctrl_l, keyboard.KeyCode(char='w') }),
+])
+def test_decode_keymapping_str_notation(keymapping: str, decoded_keymapping: List[keyboard.Key | keyboard.KeyCode]):
+    km = Keymapping()
+    assert km.decode_keymapping_str_notation(keymapping) == decoded_keymapping
